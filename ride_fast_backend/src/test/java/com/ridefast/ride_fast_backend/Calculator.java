71b4d53f5a7808e359d5dbc7d6d53d318cd013dc
Harshit Pachori <harshitpachori345@gmail.com>
2024-05-04 09:18:20 +0530

    added mockito simple calculator test

---
+++ ride_fast_backend/src/test/java/com/ridefast/ride_fast_backend/Calculator.java
@@ -0,0 +1,7 @@
+package com.ridefast.ride_fast_backend;
+
+public class Calculator {
+  public int add(int a, int b) {
+    return a + b;
+  }
+}
---
+++ ride_fast_backend/src/test/java/com/ridefast/ride_fast_backend/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.ridefast.ride_fast_backend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CalculatorTest {
+
+  @Mock
+  Calculator calculatorMock;
+
+  @BeforeEach
+  public void setUp() {
+    calculatorMock = new Calculator();
+  }
+
+  @Test
+  void testAdd_Success() {
+    int result = calculatorMock.add(3, 5);
+    assertEquals(8, result);
+  }
+
+  @Test
+  void testAdd_Failure() {
+    // Mocking the behavior of the add method to return an incorrect result
+    when(calculatorMock.add(3, 5)).thenReturn(9); // incorrect result
+    int result = calculatorMock.add(3, 5);
+
+    // Since we expect the add method to return 8, this assertion should fail
+    assertEquals(8, result);
+  }
+
+  @Test
+  void testAdd_Exception() {
+    // Test for an exception when adding large numbers
+    assertThrows(ArithmeticException.class, () -> calculatorMock.add(Integer.MAX_VALUE, 1));
+  }
+}
